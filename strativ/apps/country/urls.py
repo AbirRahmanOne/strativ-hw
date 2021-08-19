611b719c1528e147730c75da3ab8904203857057
AbirRahmanOne <abir.talks@gmail.com>
2021-08-19 17:28:28 +0600

    templates home page

--- strativ/apps/country/urls.py
+++ strativ/apps/country/urls.py
@@ -1,9 +1,10 @@
 from django.urls  import path
 
 from strativ.apps.country.views import CountryListApiView, CountryRetrieveUpdateDestroyApiView
-
+from . import views
 # Api routes
 urlpatterns = [
+    path('', views.home, name='index'),
     path('country', CountryListApiView.as_view(), name='country_list'),
     path('country/<str:pk>', CountryRetrieveUpdateDestroyApiView.as_view(), name='country_details'),
 
